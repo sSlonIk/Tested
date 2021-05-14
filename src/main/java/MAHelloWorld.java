3b908cc06fe5b0327655da017e48985e6bdb9d34
sSlonIk <kryachok@gmail.com>
2021-05-14 12:56:56 +0300

    updated hello

--- src/main/java/MAHelloWorld.java
+++ src/main/java/MAHelloWorld.java
@@ -1,8 +1,7 @@
 public class MAHelloWorld {
     public static void main(String[] args) { Hello();Mate(); }
     public static void Hello(){ System.out.print("Hello World "); }
-    public static void Mate(){
-        System.out.print("and Mate");
+    public static void Mate(){ System.out.print("and Mate");
     }
 
 }
